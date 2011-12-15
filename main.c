b2f61d049f36f932280a995638cd74ef80d903f7
John Flanagan <john@Johns-MacBook-Pro-3.local>
2011-12-15 12:14:47 -0600

    changed to ncurses to hopefully better handle text I/O

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
+#include <ncurses.h>
 
 #define INPUT_MAX_SIZE 100
 
@@ -20,55 +21,71 @@ int main() {
 	if ( serial_fd == -1 ) {
 		fprintf(stdout, "failed to open port\n");
 	}
+	else {
+		tcgetattr(serial_fd, &scorbot_config);
 
-	tcgetattr(serial_fd, &scorbot_config);
+		cfsetispeed(&scorbot_config, B9600); //set baud input to 9600
+		cfsetospeed(&scorbot_config, B9600); //set baud output to 9600
 
-	cfsetispeed(&scorbot_config, B9600); //set baud input to 9600
-	cfsetospeed(&scorbot_config, B9600); //set baud output to 9600
+		scorbot_config.c_cc[VMIN]  = 1;
+		scorbot_config.c_cc[VTIME] = 0;
 
-	scorbot_config.c_cc[VMIN]  = 1;
-	scorbot_config.c_cc[VTIME] = 0;
+		scorbot_config.c_iflag = (INPCK | PARMRK); //0x18 on OS X
+		scorbot_config.c_oflag = 0;
+		scorbot_config.c_cflag = (CLOCAL | CREAD | CSTOPB | CS8); //0x8F00 on OS X
+		scorbot_config.c_lflag = 0;
 
-	scorbot_config.c_iflag = (INPCK | PARMRK); //0x18 on OS X
-	scorbot_config.c_oflag = 0;
-	scorbot_config.c_cflag = (CLOCAL | CREAD | CSTOPB | CS8); //0x8F00 on OS X
-	scorbot_config.c_lflag = 0;
+		tcsetattr(serial_fd, TCSAFLUSH, &scorbot_config);
 
-	tcsetattr(serial_fd, TCSAFLUSH, &scorbot_config);
+		int status = TIOCM_DTR; //0x2 on OS X
+		ioctl(serial_fd, TIOCMSET, &status);
+	}
 
-	int status = TIOCM_DTR; //0x2 on OS X
-	ioctl(serial_fd, TIOCMSET, &status);
+	initscr();
+	cbreak();
+	noecho();
 
 	pid = fork(); //splits process for recieving and sending
 
 	if ( pid == 0 ) { //child process continually receives
-		fprintf(stdout, "Receiving Process Started\n");
+		if ( serial_fd == -1 ) {
+			return -1;
+		}
+
 		while ( 1 ) {
-			if ( read(serial_fd, &c, 1) > 0 ) { //if there's something from the serial port
-				fprintf(stdout, "%c", c, c);
+			if ( read(serial_fd, &c, 1) > 0 ) {
+				printw("%c", c);
 			}
 		}
 	}
 	else { //parent process sends input on return
-		fprintf(stdout, "Sending Process Started\n"); //-debug
+		fprintf(stdout, "sending process started\n"); //-debug
+		int length;
 		while ( 1 ) {
-			fgets(input, INPUT_MAX_SIZE, stdin);
-
-			int length = strlen(input);
-			input[length - 1] = '\r';
-
-			write(serial_fd, input, length);
-//			fprintf(stdout, "Sending:\n");
+			c = getch();
+			if ( c == '\04' ) {
+				break;
+			}
+			if ( c == '\n' ) {
+				c = '\r'; // replace newline with carriage return
+			}
+			printw("typed: %c - 0x%02x\n\r", c, c);
+			if ( serial_fd != -1 ) {
+			  write(serial_fd, input, length);
+			}
+//			fprintf(stdout, "sending:\n");
 //			int i;
 //			for ( i = 0; i < length; i++ ) {
-//				fprintf(stdout, "%c - %02X\n", input[i], input[i]);
+//				fprintf(stdout, "%c - %02x\n", input[i], input[i]);
 //			}
 		}
+		kill(pid);
+		wait(pid);
 	}
 
 	close(serial_fd);
 
-	fprintf(stdout, "complete");
+	fprintf(stdout, "complete\n");
 
 	return 0;
 }
